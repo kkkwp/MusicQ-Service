4c73500bc47d3534128602700c4f4546136c3f30
hb9397@naver.com <hb9397@naver.com>
2023-04-20 19:53:28 +0900

    ✨feat: 로그 아웃 및 Cookie 검증 설정 추가 [DRKKO-120] - Cookie 검증이 필요 없는 API 분리, Cookie 요청을 허용해줄 Domain 설정 - 위에서 설정된 쿠키값 검증이 필요한 모든 API의 Cookie 에서 JWT AccessToken 을 꺼내서 검증하는 설정 추가

---
+++ src/main/java/com/musicq/musicqservice/config/JwtTokenInterceptor.java
@@ -0,0 +1,47 @@
+package com.musicq.musicqservice.config;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.musicq.musicqservice.member.util.TokenProvider;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class JwtTokenInterceptor implements HandlerInterceptor {
+
+	private final TokenProvider tokenProvider;
+	private final RedisTemplate<String, String> redisTemplate;
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
+		Exception {
+		// Authorization 헤더에서 access token을 추출합니다.
+		String authorizationHeader = request.getHeader("Cookie");
+
+		if (authorizationHeader == null || !authorizationHeader.startsWith("Authorization=")) {
+			// access token이 없거나, Authorization 으로 시작하지 않으면 허용 X
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return false;
+		}
+		String accessToken = authorizationHeader.substring(14);
+		String userId = tokenProvider.getId(accessToken);
+
+		String tokenFromRedis = redisTemplate.opsForValue().get(userId);
+		String existToken = tokenFromRedis != null ? tokenFromRedis.substring(1) : null;
+
+		// access token이 redis 에 존재하는지 유무로 유효한지 확인합니다.
+		if (!accessToken.equals(existToken) || existToken.equals(null)) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return false;
+		}
+
+		// access token이 유효하면 요청을 허용합니다.
+		return true;
+	}
+}
--- src/main/java/com/musicq/musicqservice/config/WebConfig.java
+++ src/main/java/com/musicq/musicqservice/config/WebConfig.java
@@ -2,16 +2,41 @@ package com.musicq.musicqservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+	private final JwtTokenInterceptor jwtTokenInterceptor;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // 프로그램에서 제공하는 URL
-			.allowedOrigins("*") // 요청을 허용할 출처를 명시, 전체 허용
+			/*allowedOrigins("*")를 사용하여 모든 출처(Origin)를 허용하고 있기 때문에,
+			서버 측에서 Access-Control-Allow-Origin 응답 헤더의 값이 와일드카드(*)로 설정됩니다. 이 경우, 요청의 credentials 옵션이
+			"include"로 설정되어 있으면, CORS 정책에 의해 차단되기 때문에 정확한 도메인을 설정해야 한다.*/
+			.allowedOrigins("http://localhost:3000") // 요청을 허용할 출처를 명시, 전체 허용
 			.allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
-			.allowedMethods("*"); // 어떤 메서드를 허용할 것인지
+			.allowedMethods("*") // 어떤 메서드를 허용할 것인지
+			// credentials 옵션을 "include"로 설정하면, 서버로 요청 시 쿠키를 함께 보내도록 설정
+			.allowCredentials(true)
+			// 서버로 부터의 요청을 허용하는 시간 (일정 시간 지나면 제거) -> 서버 부하 감소
+			.maxAge(3600);
+	}
+
+	// Cookie 에서 AccessToken 을 꺼내서 확인하기 위한 설정으로 이 검증을 적용할 API 와 제외할 API 설정
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(jwtTokenInterceptor)
+			.addPathPatterns("/api/v1/**")
+			.excludePathPatterns("/api/v1/members/login",
+				"/api/v1/members/member",
+				"/api/v1/members/token",
+				"/api/v1/members/id/{id}",
+				"/api/v1/members/email/{email}",
+				"/api/v1/members/nickname/{id}/{nickname}",
+				"/api/v1/members/email/authentication/{email}");
 	}
 }
